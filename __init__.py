3b2779a0c164e777ff758af73391095df134b012
LoveUCB <3338561620@qq.com>
2024-05-24 23:39:01 +0800

    update

---
+++ __init__.py
@@ -0,0 +1,7 @@
+__author__ = 'Wang Jingran'
+__version__ = '1.0'
+__date__ = '2024.2.24'
+__license__ = 'GPL License'
+__email__ = 'jrwangspencer@stu.suda.edu.cn'
+__github__ = 'https://github.com/Spencer-JRWang/PDNPR'
+__department__ = "Department of Bioinformatics, Soochow University Medical College"
